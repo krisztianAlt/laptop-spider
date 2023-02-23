8c9719d793afd545fb1f4d859f985becf67d3c82
krisztianAlt <alt.krisztian@gmail.com>
2023-02-23 13:28:11 +0100

    Using proxy server - bugfix

--- proxy_spider.js
+++ proxy_spider.js
@@ -4,6 +4,7 @@ const { response } = require('express');
 
 const FREE_PROXY_LIST_MAIN_PAGE_URL = "https://free-proxy-list.net/";
 const WE_WANT_ONLY_ANONYMOUS_SERVERS = true;
+const WE_WANT_ONLY_HTTPS_COMPATIBLE_SERVERS = true;
 const PROXY_URL_SCHEME = "http://"
 
 const getHTMLCode = (url) => {
@@ -41,14 +42,15 @@ function extractProxyServerDataFromHTMLCode(pageBody) {
         let serverIpAddress = $(this).find("td:nth-child(1)").text().trim();
         let serverPortNumber = $(this).find("td:nth-child(2)").text().trim();
         let serverAnonymity = $(this).find("td:nth-child(5)").text().trim();
+        let serverHTTP = $(this).find("td:nth-child(7)").text().trim();
         
         let proxyURL = PROXY_URL_SCHEME + serverIpAddress.toString() + ':' + serverPortNumber.toString()
 
-        if (WE_WANT_ONLY_ANONYMOUS_SERVERS) {
-            if (serverAnonymity=="anonymous") {
+        if (WE_WANT_ONLY_ANONYMOUS_SERVERS && WE_WANT_ONLY_HTTPS_COMPATIBLE_SERVERS) {
+            if (serverAnonymity=="anonymous" && serverHTTP=="yes") {
                 availableProxyServers.push(proxyURL);
             }
-        } else{
+        } else {
             availableProxyServers.push(proxyURL);
         }
         
